6abc43f744fdf32a41cfa6c4438d9c0e1c6545b9
agit6 <aigt6bigdata@163.com>
2022-11-05 22:40:00 +0800

    version03

--- src/main/java/algorithm/Demo05_BinarySearch.java
+++ src/main/java/algorithm/Demo05_BinarySearch.java
@@ -10,6 +10,8 @@ public class Demo05_BinarySearch {
 
         int index = BinarySearch(arr, 5);
 
+        System.out.println("yes i can");
+
         System.out.println((index == -1)?"该数不存在":"该数的下标为：" + index);
 
     }
